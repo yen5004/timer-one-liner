21003abd6ff76cb7f93584c0bb5e8206511d157a
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-10-18 08:03:11 -0400

    Update timr.sh

--- timr.sh
+++ timr.sh
@@ -11,16 +11,13 @@ if [[ -z "$duration" ]]; then
   exit 1
 fi
 
-# Convert duration to minutes and seconds
-#minutes=$((duration / 60))
-#seconds=$((duration % 60))
-
+# for loop for execution of code
 for ((i=duration; i>0; i--)); do
-  # Calculate remaining minutes and seconds
-  remaining_mins=$((i / 60))
-  remaining_secs=$((i % 60))
-
-  echo -ne "Time remaining: $remaining_mins minutes and $remaining_secs seconds\033[OK\r"
-  sleep 1
+    # Calculate remaining minutes and seconds
+    remaining_minutes=$((i / 60))
+    remaining_seconds=$((i % 60))
+    
+    echo -ne "Time remaining: $remaining_minutes minutes and $remaining_seconds seconds\033[0K\r"
+    sleep 1
 done
-echo "" $ Newline for better output formatting
+echo ""  # Newline for better output formatting
